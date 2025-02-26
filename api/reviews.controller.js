e9441d967301e5366a03df81f27698bb2082d8f4
Bateyjosue <josuebatey19@gmail.com>
2025-02-26 03:03:17 +0200

    feat:update controller to add really db manipulation from the DAO

--- api/reviews.controller.js
+++ api/reviews.controller.js
@@ -1,7 +1,41 @@
-import reviewsDAO from "../dao/reviewsDAO.js"
+import ReviewsDAO from "../dao/reviews.dao.js"
 class ReviewsController {
   constructor() { }
   static async getReview(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let review = await ReviewsDAO.getReview(id)
+
+      if (!review) {
+        res.status(404).json({ error: "not found" })
+        return
+      }
+      res.json(review)
+    } catch (err) {
+      console.log(`api ${err}`);
+      res.status(500).json({ error: err.message })
+    }
+  }
+
+  static async getReviews(req, res, next) {
+    try {
+      let id = req.params.id || {}
+      let reviews = await ReviewsDAO.getReviewsByMovieId(id)
+
+      if (!reviews) {
+        res.status(404).json({ error: "Not found" })
+        return
+      }
+      res.json(reviews)
+    }
+    catch (err) {
+      console.log(`api ${err}`);
+      res.status(500).json({ error: err })
+    }
+  }
+
+
+  static async postReview(req, res, next) {
     try {
       const movieID = req.body.movieId
       const review = req.body.review
@@ -14,20 +48,39 @@ class ReviewsController {
       res.status(500).json({ error: err.message })
     }
   }
+  static async updateReview(req, res, next) {
+    try {
+      const reviewId = req.params.id
+      const review = req.body.review
+      const user = req.body.user
 
-  getReviews(req, res) {
-    return res.send(reviews);
+      const reviewResponse =  await ReviewsDAO.updateReview(reviewId, user, review)
+
+      let { error } = reviewResponse
+      if (error) {
+        res.status(404).json({ error })
+      }
+
+      if (reviewResponse.modifiedCount === 0) {
+        throw new Error(
+          "unable to update review ",
+        )
+      }
+
+      res.json({ status: "success" })
+    } catch (err) {
+      res.status(500).json({ error: err.message })
+    }
   }
-
-
-  postReview(req, res) {
-
-  }
-  updateReview(req, res) {
-
-  }
-  deleteReview(req, res) {
-
+  static async deleteReview(req, res, next) {
+    try {
+      const reviewId = req.params.id
+      const reviewResponse = await ReviewsDAO.deleteReview(reviewId)
+      res.json({status: "success"})
+    }
+    catch (err) {
+      res.status(500).json({ error: err.message })
+    }
   }
 }
 
